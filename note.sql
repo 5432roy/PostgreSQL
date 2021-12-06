57c6fbb4d60fd9433812e46f8f39eb65f2a4d1bc
5432roy <960289@gmail.com>
2021-12-06 00:19:42 -0800

    the end for 12/5

--- note.sql
+++ note.sql
@@ -34,8 +34,78 @@ WHERE id = 2;
 DELETE FROM note
 WHERE id = 1;
 
--- * means select everything from table_name
+-- * means select everything (column) from table_name
 SELECT * FROM note;
 
+-- select some specific properties from table, using comma to seperate them
+SELECT name, email
+FROM note;
 
+-- select a column and temperory rename that with new name
+-- the actual property in the table is still the same
+SELECT name AS "newName"
+FROm note;
 
+-- modify the table name, comment out this line for easy coding 
+--ALTER TABLE note
+--RENAME TO new_note;
+
+-- "select distinct" filters out all duplicate values in the specified column
+SELECT DISTINCT id
+FROM note;
+
+-- select things with specific condition
+SELECT * FROM note
+WHERE id < 10;
+
+-- "LIKE" command, '_' is a wildcard character, which can substitute any individual character here without breaking the pattern
+-- "LIKE" is not case sensitive
+SELECT * FROM note
+-- such as "R3y", "Rdy", "Roy" is fitting the condition
+WHERE name LIKE 'R_y';
+
+-- '%' is also a wildcard character, which can replace an enitre missing string or character
+SELECT * FROM movies
+-- "123man456", "man__ 25", "Man_123" all can fit
+WHERE name LIKE '%man%';
+
+-- to statement for NULL >> "IS" or "IS NOT"
+SELECT * FROM note
+WHERE email IS NOT NULL;
+
+-- "BETWEEN ___ AND ___" both properties are included, for instance, BETWEEN 100 AND 200, is the range from 100 - 200
+-- this is a case sensetive statement
+SELECT * FROM note
+-- However, for this condition, only the things with exact 'R' will fit the statement, 'Ra' or anything like this are excluded.
+-- my own thought, this is according to the lexiogical order
+WHERE name BETWEEN 'A' AND 'R';
+
+-- AND operator
+-- OR is the same
+SELECT * FROM note
+WHERE id < 1985
+    AND email LIKE '%@gmail.com';
+
+-- ORDER BY 
+SELECT * FROM note
+WHERE id < 10
+-- DESC means order by the descending order, ASC means the asscending order (default)
+ORDER BY name DESC;
+
+-- Limit
+SELECT * FROM note
+ORDER BY id DESC
+LIMIT 3;
+
+-- "CASE", usually use in SELECT statement, can think like if else statement
+SELECT name,
+    CASE
+        -- if id == 1
+        WHEN id = 1 THEN 'cool'
+        -- else if 
+        WHEN id = 2 THEN 'not so cool'
+        ELSE 'So bad'
+    -- output a new column with this "New property"
+    -- 'cool', 'not so cool'... etc will be in here
+    END AS "New property"
+FROM note;
